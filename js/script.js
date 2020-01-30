8682a3046a9c1645ebccd7101301154611f0096a
Soimuen11 <soimuen11@gmail.com>
2020-01-30 12:50:10 +0100

    finished GoL

--- js/script.js
+++ js/script.js
@@ -1,113 +1,87 @@
-//function game_loop(){
-//
-//}
+'use strict'
 
-function new_gen(gen){
-		gen++
-		return gen
-}
-
-function init_board(rows, cols, alive){
-		grid = [];
-		let nb_rows = rows
-		let nb_cols = cols
-		for (let i = 1; i <= nb_rows;i++){
-				for(let j = 1; j <= nb_cols; j++){
-						let square = {row: i, col: j, type: false}
-						grid.push(square);
+function create_grid(rows, cols, alive){
+		let grid = new Array()
+		for (let i = 0; i < rows;i++){
+				grid[i] = new Array()
+				for(let j = 0; j < cols; j++){
+						grid[i][j] = false
 				}
 		}
+		while (alive > 0) {
+				let i_r = Math.floor(Math.random() * rows)
+				let j_r = Math.floor(Math.random() * cols);
+				if (grid[i_r][j_r] === false){
+						grid[i_r][j_r] = true;
+						alive--
+				}
+		}
+		console.log(grid)
 		return grid;
 }
 
 function draw(grid){
+		console.log(grid)
 		let board = $('<table>');
-		for (let i = 1; i <= nb_rows;i++){
+		for (let i = 0; i < grid.length ;i++){
 				let tr = $('<tr>');
-				for(let j = 1; j <= nb_cols; j++){
+				for(let j = 0; j < grid[i].length; j++){
 						let td= $('<td>');
 						let tdId=`${i}-${j}`;	
-						$('<td>')
+						$(td)
 								.attr('id', tdId)
 								.appendTo(tr)
+						if (grid[i][j] === true){
+								$(td).addClass('black')
+						}
 				}
 				$(board).append(tr);
 		}
 		$('#board').html(board);
-		let draw_alive = alive;
-		while (draw_alive > 0){
-				let random = Math.floor(Math.random() * grid.length)
-				if (grid[random].type === true){
-						$('td#' + grid[random].row + '-' + grid[random].col).addClass('black')
-						draw_alive--
+		return board
+}
+
+function is_inside(i, j, rows, cols) {
+		return i >= 0 && i < rows && j >= 0 && j < cols
+}
+
+function next_gen(grid){
+		let new_grid = Array()
+		for (let i = 0; i < grid.length; i++){
+				new_grid[i] = new Array()
+				for (let j = 0; j < grid[i].length; j++){
+						let dirs = [ [0,1], [0,-1], [1,0], [-1,0], [1,1], [-1,-1], [1,-1], [-1,1] ] 
+						let neighbors = 0;
+						for (let dir of dirs){
+								if (is_inside(i + dir[0], j + dir[1], grid.length, grid[i].length) && grid[i + dir[0]][j + dir[1]] === true) {
+										neighbors++
+								}
+						}
+						new_grid[i][j] = born_or_survive(grid[i][j], neighbors)
 				}
+		}
+		return new_grid
+}
+
+function born_or_survive(alive, neighbors){
+		if (alive === true) {
+				return (neighbors === 2 || neighbors === 3)
+		} else {
+				return neighbors === 3
 		}
 }
 
-
-// WE START FROM A GIVEN SQUARE WITH ROW,COL && WE CHECK THE SQUARES AROUND ONE SQ 
-//function find_neighbors (grid){
-//		let n = 0
-//		let neighbors = 0;
-//		while (n < grid.length){
-//				let row = grid[n].row
-//				let col = grid[n].col
-//				// check up, down, right, left + diagonals
-//				let dirs = [ [0,1], [0,-1], [1,0], [-1,0] [1,1], [-1,-1], [1,-1], [-1,1] ] 
-//				// check in 8 directions
-//				for (let dir of dirs){
-//						//check neighbors with range 1
-//						for (let i=0; i<=1; i++){
-//								let checked_row = grid[n].row + i*dirs[0];
-//								let checked_col = grid[n].col + i*dirs[1];
-//								let checked_type = grid[n].type;
-//								if (is_inside(checked_row, checked_col) && checked_type === true){
-//										neighbors++
-//								}else{
-//										break;
-//								}
-//						}
-//				}
-//				n++
-//		}
-//		return neighbors;
-//}
-
-function is_inside(row, col){
-		return !(row < 1 || row > nb_rows || col < 1 || col > nb_cols)
-}
-
-// function born_or_survive(neighbors){
-// 		if (neighbors == 2 || neighbors == 3
-// 				&& grid[square].type === true){
-// 				grid[square].type === true;
-//				alive++;
-// 		} 
-// 		else if(neighbors == 3 && grid[square].type === false){
-// 				grid[square].type === true;
-//				alive++;
-// 		}
-// 		else if(neighbors < 2 || neighbors > 3 && grid[square].type === true){
-// 				grid[square].type === false;
-//				alive--;
-// 		}
-//}
-//
-//
-//let delay = setInterval(function (event) {
-// 						new_gen(0)
-// 					//	if (alive == 0) {
-// 					//			clearInterval(delay)
-// 					//	}
-//}, 50, this)
-
-
 $(document).ready(function(){
-		//FIRST, YOU INITALIZE THE BOARD WITH THE NB OF ROWS AND COLS THAT YOU WANT
 		$('#init').on('click', function(){
 				let rows = parseInt(prompt('how many rows do you want?'))
 				let	cols = parseInt(prompt('how many cols do you want?'))
 				let	alive = parseInt(prompt('how many living cells do you want?'))
-				init_board(rows, cols, alive);
+				let grid = create_grid(rows, cols, alive);
+				draw(grid)
+				let new_grid = next_gen(grid)
+				let delay = setInterval(function (event){
+						new_grid = next_gen(new_grid)
+						draw(new_grid)
+				}, 200, this)
 		});
 });
