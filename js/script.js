f051f618bb35661a704ede67026a5a195eb5f626
Phil Wayne <soimuen11@gmail.com>
2021-08-09 11:18:00 +0200

    fixed indentation

--- js/script.js
+++ js/script.js
@@ -21,121 +21,121 @@ function create_grid(rows, cols, alive){
 }
 
 function draw(grid){
-		let board = $('<table>');
-		for (let i = 0; i < grid.length ;i++){
-				let tr = $('<tr>');
-				for(let j = 0; j < grid[i].length; j++){
-						let td= $('<td>');
-						let tdId=`${i}-${j}`;	
-						$(td)
-								.attr('id', tdId)
-								.appendTo(tr)
-						if (grid[i][j] === true){
-								$(td).addClass('black')
-						}
-				}
-				$(board).append(tr);
+	let board = $('<table>');
+	for (let i = 0; i < grid.length ;i++){
+		let tr = $('<tr>');
+		for(let j = 0; j < grid[i].length; j++){
+			let td= $('<td>');
+			let tdId=`${i}-${j}`;	
+			$(td)
+				.attr('id', tdId)
+				.appendTo(tr)
+			if (grid[i][j] === true){
+				$(td).addClass('black')
+			}
 		}
-		$('#board').html(board);
-		return board
+		$(board).append(tr);
+	}
+	$('#board').html(board);
+	return board
 }
 
 function is_inside(i, j, rows, cols) {
-		return i >= 0 && i < rows && j >= 0 && j < cols
+	return i >= 0 && i < rows && j >= 0 && j < cols
 }
 
 function is_alive(grid, i, j) {
-		if (grid[i] != undefined && is_inside(i, j, grid.length, grid[i].length)) {
-				return grid[i][j]
+	if (grid[i] != undefined && is_inside(i, j, grid.length, grid[i].length)) {
+		return grid[i][j]
+	} else {
+		if (i === -1) {
+			if (j === -1) {
+				return grid[grid.length - 1][(grid[grid.length - 1].length - 1)]
+			} else if (j === grid[grid.length - 1].length) {
+				return grid[grid.length - 1][0]
+			} else {
+				return grid[grid.length - 1][j]
+			}
+		} else if (i === grid.length) {
+			if (j === -1) {
+				return grid[0][grid[0].length - 1]
+			} else if (j === grid[0].length) {
+				return grid[0][0]
+			} else {
+				return grid[0][j]
+			}
 		} else {
-				if (i === -1) {
-						if (j === -1) {
-								return grid[grid.length - 1][(grid[grid.length - 1].length - 1)]
-						} else if (j === grid[grid.length - 1].length) {
-								return grid[grid.length - 1][0]
-						} else {
-								return grid[grid.length - 1][j]
-						}
-				} else if (i === grid.length) {
-						if (j === -1) {
-								return grid[0][grid[0].length - 1]
-						} else if (j === grid[0].length) {
-								return grid[0][0]
-						} else {
-								return grid[0][j]
-						}
-				} else {
-						if (j === -1) {
-								return grid[i][(grid[i].length - 1)]
-						} else if (j === grid[i].length) {
-								return grid[i][0]
-						} else {
-								return grid[i][j]
-						}
-				}
+			if (j === -1) {
+				return grid[i][(grid[i].length - 1)]
+			} else if (j === grid[i].length) {
+				return grid[i][0]
+			} else {
+				return grid[i][j]
+			}
 		}
+	}
 }
 
 function next_gen(grid){
-		let new_grid = Array()
-		for (let i = 0; i < grid.length; i++){
-				new_grid[i] = new Array()
-				for (let j = 0; j < grid[i].length; j++){
-						let dirs = [ [0,1], [0,-1], [1,0], [-1,0], [1,1], [-1,-1], [1,-1], [-1,1] ] 
-						let neighbors = 0;
-						for (let dir of dirs){
-								if (is_alive(grid, i + dir[0], j + dir[1])) {
-										neighbors++
-								}
-						}
-						new_grid[i][j] = born_or_survive(grid[i][j], neighbors)
+	let new_grid = Array()
+	for (let i = 0; i < grid.length; i++){
+		new_grid[i] = new Array()
+		for (let j = 0; j < grid[i].length; j++){
+			let dirs = [ [0,1], [0,-1], [1,0], [-1,0], [1,1], [-1,-1], [1,-1], [-1,1] ] 
+			let neighbors = 0;
+			for (let dir of dirs){
+				if (is_alive(grid, i + dir[0], j + dir[1])) {
+					neighbors++
 				}
+			}
+			new_grid[i][j] = born_or_survive(grid[i][j], neighbors)
 		}
-		return new_grid
+	}
+	return new_grid
 }
 
 function born_or_survive(alive, neighbors){
-		if (alive === true) {
-				return (neighbors === 2 || neighbors === 3)
-		} else {
-				return neighbors === 3
-		}
+	if (alive === true) {
+		return (neighbors === 2 || neighbors === 3)
+	} else {
+		return neighbors === 3
+	}
 }
 
 $(document).ready(function(){
-				$('#draw').on('click', function(){
-						let counter = 0;
-						let row = parseInt($('#row').val())
-						let col = parseInt($('#col').val())
-						let alive = parseInt($('#alive').val())
-						let grid = create_grid(row, col, alive);
-						draw(grid)
-						$('td').on('click', function(event){
-								if ($(this).hasClass('black')){
-										$(this).removeClass('black');
-										alive++
-								}else if (alive > 0) {
-										$(this).addClass('black');
-										alive--
-								}
-						});
-						$('#start').on('click', function(){
-								for (let i=0; i < grid.length; i++){
-										for (let j=0; j < grid[i].length; j++){
-												grid[i][j] = $('td#' + `${i}-${j}`).hasClass('black')
-										}
-								}
-								let new_grid = next_gen(grid)
-								draw(new_grid)
-								let delay = setInterval(function (event){
-										new_grid = next_gen(new_grid)
-										draw(new_grid)
-										counter++
-										$('#counter').html('Generation: ' + counter)
-								}, 100, this)
-								$('#stop').on('click', function(){
-										clearInterval(delay);
-								});
-						});
-				});
+	$('#draw').on('click', function(){
+		let counter = 0;
+		let row = parseInt($('#row').val())
+		let col = parseInt($('#col').val())
+		let alive = parseInt($('#alive').val())
+		let grid = create_grid(row, col, alive);
+		draw(grid)
+		$('td').on('click', function(event){
+			if ($(this).hasClass('black')){
+				$(this).removeClass('black');
+				alive++
+			}else if (alive > 0) {
+				$(this).addClass('black');
+				alive--
+			}
+		});
+		$('#start').on('click', function(){
+			for (let i=0; i < grid.length; i++){
+				for (let j=0; j < grid[i].length; j++){
+					grid[i][j] = $('td#' + `${i}-${j}`).hasClass('black')
+				}
+			}
+			let new_grid = next_gen(grid)
+			draw(new_grid)
+			let delay = setInterval(function (event){
+				new_grid = next_gen(new_grid)
+				draw(new_grid)
+				counter++
+				$('#counter').html('Generation: ' + counter)
+			}, 100, this)
+			$('#stop').on('click', function(){
+				clearInterval(delay);
+			});
+		});
+	});
 });
